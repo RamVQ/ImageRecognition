713d3f34926d4db7a6b7b32bc492660cdc03680d
RamVQ <rvq25@hotmail.com>
2018-02-02 08:32:30 -0600

    added affine trans

--- CODE/OPENCV/image_basics/affine_transform_image.py
+++ CODE/OPENCV/image_basics/affine_transform_image.py
@@ -5,14 +5,15 @@ import matplotlib
 matplotlib.use('Qt5Agg')
 import matplotlib.pyplot as plt
 
-img = cv.imread('../../../IMAGES/perspective_input.png')
+img = cv.imread('../../../IMAGES/affine_input.png')
 rows,cols,ch = img.shape
 
-pts1 = np.float32([[29,200],[208,202],[42,42],[197,37]])
-pts2 = np.float32([[0,300],[300,300],[0,0],[300,0]])
+pts1 = np.float32([[42,150],[42,44],[146,44]])
+pts2 = np.float32([[50,200],[10,100],[100,20]])
 
-M = cv.getPerspectiveTransform(pts1,pts2)
-dst = cv.warpPerspective(img,M,(300,300))
+M = cv.getAffineTransform(pts1,pts2)
+
+dst = cv.warpAffine(img,M,(cols,rows))
 
 plt.subplot(121),plt.imshow(img),plt.title('Input')
 plt.subplot(122),plt.imshow(dst),plt.title('Output')
